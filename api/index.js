d05a8aab884320fb48c759fbd8b35e2a1e626f27
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-12 15:00:48 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 
+import userRouter from "./routes/user.route.js";
+
 main();
 const app = express();
 
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT_NUM, () => {
   console.log(`server running on port number ${process.env.PORT_NUM}`);
