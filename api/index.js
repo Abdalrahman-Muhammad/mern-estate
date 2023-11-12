0d27faf41c733bf9a0320e08bc14a46e785de2b1
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-12 14:08:10 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
+main();
 const app = express();
 
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-const PORT_NUM = 3000;
-app.listen(PORT_NUM, () => {
-  console.log(`server running on port number ${PORT_NUM}`);
+app.listen(process.env.PORT_NUM, () => {
+  console.log(`server running on port number ${process.env.PORT_NUM}`);
 });
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`DB connected successfully`);
+  } catch (error) {
+    console.log("error while connecting to database:", error.message);
+  }
+}
