6c4d56cf0985b235a22925be2c0ff789e199ae08
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-17 00:55:25 +0200

    fix bugs in verfiy token functi0on

--- api/utils/verfiyToken.js
+++ api/utils/verfiyToken.js
@@ -3,10 +3,10 @@ import { errorHandler } from "./error.js";
 
 export const verfiyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(new errorHandler(401, "Unautorized"));
+  if (!token) return next(errorHandler(401, "Unautorized"));
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(new errorHandler(403, "forbidden"));
+    if (err) return next(errorHandler(403, "forbidden"));
     req.user = user;
     next();
   });
