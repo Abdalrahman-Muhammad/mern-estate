aeb757fc0a8f3721cb62fe3254c4e0d56088c4ee
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-25 13:38:22 +0200

    create search api route

--- api/routes/listing.router.js
+++ api/routes/listing.router.js
@@ -5,6 +5,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
@@ -12,4 +13,5 @@ router.post("/create", verfiyToken, createListing);
 router.delete("/delete/:id", verfiyToken, deleteListing);
 router.post("/update/:id", verfiyToken, updateListing);
 router.get("/get/:id", verfiyToken, getListing);
+router.get("/get", getListings);
 export default router;
