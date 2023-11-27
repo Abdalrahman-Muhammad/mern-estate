100c8f22b094033d7f9a7a17a297af08c9bd8e2b
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-27 07:55:45 +0200

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from "path";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.router.js";
@@ -10,9 +10,9 @@ import listingRouter from "./routes/listing.router.js";
 
 //database connection
 main();
+const __dirname = path.resolve();
 const app = express();
 
-// app.use(cors());
 //body parser
 app.use(express.json());
 
@@ -22,6 +22,11 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // global error handler
 app.use((err, req, res, next) => {
