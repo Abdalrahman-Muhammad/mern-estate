6b10e98cac62b913e092c164d97abf36206ae121
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2023-11-27 08:10:18 +0200

    fix index.js file for sign in

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,7 @@ import { About } from "./About";
 import { Home } from "./Home";
 import { Profile } from "./Profile";
 import { SignUp } from "./SignUp";
-import { SignIn } from "./SignIn.JSX";
+import { SignIn } from "./SignIn";
 import { CreateListing } from "./CreateListing";
 import { UpdateListing } from "./UpdateListing";
 import { Listing } from "./Listing";
